708f5af0251dab138371336f6093df022e9f99b4
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2021-10-18 07:25:06 -0700

    added javascript to make cards expand when clicked.

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClassees();
+        panel.classList.add("active")
+    })
+})
+
+function removeActiveClassees() {
+    panels.forEach((panel) => {
+panel.classList.remove("active");
+    })
+}
